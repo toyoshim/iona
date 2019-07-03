a55e4335cba9929b4af0cafac84d319f51dec079
Takashi Toyoshima <toyoshim@gmail.com>
2019-07-03 22:39:19 +0900

    jvsio roll

--- iona.ino
+++ iona.ino
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "jvsio/NanoClient.cpp"
+#include "jvsio/clients/NanoClient.cpp"
 #include "jvsio/JVSIO.cpp"
 
 NanoDataClient data;
