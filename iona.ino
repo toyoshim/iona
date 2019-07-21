5173e5a44c0143827b9db4175fa350eab56aa7bc
Takashi Toyoshima <toyoshim@gmail.com>
2019-07-22 01:52:49 +0900

    Coin unit number should start from 1 rather than 0

--- iona.ino
+++ iona.ino
@@ -220,7 +220,7 @@ void loop() {
     }
     break;
    case JVSIO::kCmdCoinSub:
-    if (!data[1])
+    if (data[1] == 1)
       coinCount -= data[3];
     io.pushReport(JVSIO::kReportOk);
     break;
